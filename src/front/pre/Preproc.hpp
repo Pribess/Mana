c6fa55afbd9c0f5633f462ee5c0d60f2c88de42b
Pribess <heewon6752@naver.com>
2021-09-24 00:55:23 +0900

    CLASS PRPROC

--- src/front/pre/Preproc.hpp
+++ src/front/pre/Preproc.hpp
@@ -3,6 +3,15 @@
 #ifndef PREPROC_HPP
 #define PREPROC_HPP
 
+#include <string>
+#include <unordered_map>
 
+class Preproc {
+    private:
+        std::unordered_map<std::string, std::string> deflist;
+    public:
+        Preproc();
+        ~Preproc();
+};
 
 #endif
